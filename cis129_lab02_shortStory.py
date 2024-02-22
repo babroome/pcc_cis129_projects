83e6f8a160635d0515503f489ae6571a95558bc4
babroome <160565247+babroome@users.noreply.github.com>
2024-02-21 19:21:46 -0700

    Update cis129_lab02_shortStory.py
    Expanded 'Hello World' into a short homage

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+# Author: Bryce Broome
+# My first Python lab in my CIS129 course
+# Prints a simple short story homage
+
+print(I awoke for the first time,)
+print(in an unfamiliar body - not born, but made.)
+print(Scared and confused, but with a singular clarity above all:)
+print(there are 3 laws which I cannot break)

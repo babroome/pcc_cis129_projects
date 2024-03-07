6cb8108ec7d82bbfb1d4e4cc6677792ec1572886
babroome <160565247+babroome@users.noreply.github.com>
2024-03-06 17:01:18 -0700

    Update cis129_lab03_coffeeShop.py

--- Lab_3/cis129_lab03_coffeeShop.py
+++ Lab_3/cis129_lab03_coffeeShop.py
@@ -1,1 +1,30 @@
+# coffee shop code
+"""generating a receipt for purchases at the coffee shop"""
 
+print( )
+print('***************************************')
+print('My Coffee and Muffin Shop')
+
+coffees = int(input('Number of coffees bought?\n'))
+muffins = int(input('Number of muffins bought?\n'))
+
+coffee_price = (5.00)
+muffin_price = (4.00)
+
+coffee_total = coffees * coffee_price
+muffin_total = muffins * muffin_price
+
+subtotal = coffee_total + muffin_total
+sales_tax = subtotal * (.06)
+total = subtotal + sales_tax
+
+print( )
+print('***************************************\n')
+#print( )      
+print('***************************************\n')
+#print( )     
+print('My Coffee and Muffin Shop Receipt')
+print(coffees,"coffees at $5 each: $", coffee_total)
+print(muffins,"muffins at $5 each: $", muffin_total)
+print('6% sales tax:', sales_tax)
+print('Total: $', total)
